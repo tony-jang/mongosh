b03620d241299fc3902470a41c718b3a67e6ba60
Rhys Howell <rhys@rhysh@live.com>
2020-06-22 15:25:59 +0200

    Removed unneeded global and extra tests

--- packages/shell-api/src/decorators.ts
+++ packages/shell-api/src/decorators.ts
@@ -38,12 +38,7 @@ interface Signatures {
   [key: string]: TypeSignature;
 }
 
-const signaturesGlobalIdentifier = '@@@mdb.signatures@@@';
-if (!global[signaturesGlobalIdentifier]) {
-  global[signaturesGlobalIdentifier] = {};
-}
-
-const signatures: Signatures = global[signaturesGlobalIdentifier];
+const signatures: Signatures = {};
 
 export const toIgnore = ['toReplString', 'shellApiType', 'constructor'];
 export function shellApiClassDefault(constructor: Function): void {
