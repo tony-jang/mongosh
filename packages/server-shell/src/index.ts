ed79514f50945a76cd488321391a04ed9b807d7b
Tony Jang <tony@chequer.io>
2020-07-10 10:59:55 +0900

    Remove Invalid import

--- packages/server-shell/src/index.ts
+++ packages/server-shell/src/index.ts
@@ -1,18 +1,14 @@
-import CliRepl from './cli-repl';
 import parseCliArgs from './arg-parser';
 import mapCliToDriver from './arg-mapper';
 import completer from './completer';
 import clr from './clr';
 import { USAGE, TELEMETRY, MONGOSH_WIKI } from './constants';
 
-export default CliRepl;
-
 export {
   clr,
   USAGE,
   TELEMETRY,
   MONGOSH_WIKI,
-  CliRepl,
   completer,
   parseCliArgs,
   mapCliToDriver
