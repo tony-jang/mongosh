5fe231860ea1ddb10669c336c489b8f0da95878c
Tony Jang <tony@chequer.io>
2020-07-10 18:28:09 +0900

    Change port to 58410

--- packages/server-shell/src/run.ts
+++ packages/server-shell/src/run.ts
@@ -3,8 +3,9 @@ import { requestListener } from './server';
 
 try {
   process.title = 'mongosh_server';
-  http.createServer(requestListener).listen(25849, '127.0.0.1');
-  console.log(`Server is listening on ${25849}`);
+  const port = 58410;
+  http.createServer(requestListener).listen(port, '127.0.0.1');
+  console.log(`Server is listening on ${port}`);
 } catch (e) {
   // eslint-disable-next-line no-console
   console.log(e.message);
